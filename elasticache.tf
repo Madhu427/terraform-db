19be8879816cd1dc2badd5d25a1aa65fa14bebf0
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-21 17:56:04 +0530

    "redis-${var.ENV}"

--- elasticache.tf
+++ elasticache.tf
@@ -1,9 +1,8 @@
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "redis.${var.ENV}"
+  cluster_id           = "redis-${var.ENV}"
   engine               = "redis"
   node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
-#  parameter_group_name = "default.redis6.x"
   engine_version       = "6.x"
   port                 = 6379
   subnet_group_name = "aws_elasticache_subnet_group.subnet-group.name"
@@ -11,7 +10,7 @@ resource "aws_elasticache_cluster" "redis" {
 }
 
 resource "aws_elasticache_subnet_group" "subnet-group" {
-  name       = "redis.${var.ENV}"
+  name       = "redis-${var.ENV}"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
 }
 
