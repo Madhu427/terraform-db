9097f48ed39abf8b826120e7943ce472f614b64d
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-21 19:43:02 +0530

    aws_db_subnet_group.default.name

--- elasticache.tf
+++ elasticache.tf
@@ -5,8 +5,8 @@ resource "aws_elasticache_cluster" "redis" {
   num_cache_nodes      = 1
   engine_version       = "6.x"
   port                 = 6379
-  subnet_group_name = "aws_elasticache_subnet_group.subnet-group.name"
-  security_group_ids = [aws_security_group.redis.id]
+  subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
+  security_group_ids   = [aws_security_group.redis.id]
 }
 
 resource "aws_elasticache_subnet_group" "subnet-group" {
